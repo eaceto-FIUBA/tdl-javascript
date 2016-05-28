e73e0dcd9164fa24178137bc79858f20d55e7d2c
Ezequiel Aceto <eaceto@users.noreply.github.com>
2016-05-28 12:57:07 -0300

    Serve index.html

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// make express look in the public directory for assets (css/js/img/html)
+// make express look in the client directory for assets (css/js/img/html)
 app.use(express.static(__dirname + '/client'));
 
 // set the home page route
 app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Server running on http://localhost:' + port);
 });
