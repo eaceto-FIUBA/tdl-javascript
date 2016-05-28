b8c40825f833a0fe0515b6af72584a1234e84762
Ezequiel Aceto <eaceto@users.noreply.github.com>
2016-05-28 14:49:23 -0300

    API: Added socket.io ID to Join endpoint

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 ### Crea un usuario en la sala de chat
 
-    PUT         /api/user/:username/join
+    PUT         /api/user/:username/join/:socket
 
 Si el usuario pudo ser creado devuelve
 
-    STATUS CODE 201     {"username": <username>}
+    STATUS CODE 201     {"username": <username>, "socket": <socket.io id>}
 
 Si el usuario no pudo ser creado devuelve
 
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // set the port of our application
 var port = process.env.PORT || 8080;
@@ -8,14 +10,16 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/client'));
 
 var users = {};
+var sockets = {};
 
 // set the home page route
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-app.put('/api/user/:username/join', function(req, res) {
+app.put('/api/user/:username/join/:socket', function(req, res) {
     var username = req.params.username;
+    var socket = req.params.socket;
     if (username in users) {
         var e = {"error": "El usuario '" + username + "' ya existe en la sala."};
         res.json(e);
@@ -23,8 +27,9 @@ app.put('/api/user/:username/join', function(req, res) {
         return;
     }
     console.log("[JOIN]\t" + username);
-    var e = {"username": username};
-    users[username] = true;
+    var e = {"username": username, "socket": socket};
+    users[username] = socket;
+    sockets[socket] = username;
     res.json(e);
     res.status(201);
 });
@@ -33,12 +38,18 @@ app.delete('/api/user/:username/leave', function(req, res) {
     var username = req.params.username;
     if (username in users) {
         console.log("[LEAVE]\t" + username);
+        var socket = users[username];
         delete users[username];
+        delete sockets[socket];
         res.status(200);
         res.end();
         return;
     }
     res.status(404);
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
 });
 
 
