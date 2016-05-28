14c2d37fbd472fd68d861b3efda763febbb5fa8c
Ezequiel Aceto <eaceto@users.noreply.github.com>
2016-05-28 14:36:09 -0300

    API: User can Join and Leave

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# tdl-javascript
-Proyecto de Ejemplo para el curso de Teoria del Lenguaje
+# TDL Char
+
+## API
+
+### Crea un usuario en la sala de chat
+
+    PUT         /api/user/:username/join
+
+Si el usuario pudo ser creado devuelve
+
+    STATUS CODE 201     {"username": <username>}
+
+Si el usuario no pudo ser creado devuelve
+
+    STATUS CODE 409     {"error": "El usuario '<username>' ya existe en la sala."}
+
+
+### Borra a un usuario de la sala de chat
+
+    DELETE      /api/user/:username/leave
+
+Si el usuario pudo ser borrado devuelve
+
+    STATUS CODE 200
+
+Si el usuario no pudo ser borrado devuelve
+
+    STATUS CODE 404
+
--- app.js
+++ app.js
@@ -7,10 +7,40 @@ var port = process.env.PORT || 8080;
 // make express look in the client directory for assets (css/js/img/html)
 app.use(express.static(__dirname + '/client'));
 
+var users = {};
+
 // set the home page route
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/client/index.html'));
 });
+
+app.put('/api/user/:username/join', function(req, res) {
+    var username = req.params.username;
+    if (username in users) {
+        var e = {"error": "El usuario '" + username + "' ya existe en la sala."};
+        res.json(e);
+        res.status(409);
+        return;
+    }
+    console.log("[JOIN]\t" + username);
+    var e = {"username": username};
+    users[username] = true;
+    res.json(e);
+    res.status(201);
+});
+
+app.delete('/api/user/:username/leave', function(req, res) {
+    var username = req.params.username;
+    if (username in users) {
+        console.log("[LEAVE]\t" + username);
+        delete users[username];
+        res.status(200);
+        res.end();
+        return;
+    }
+    res.status(404);
+});
+
 
 app.listen(port, function() {
     console.log('Server running on http://localhost:' + port);
